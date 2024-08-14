0af59800a43ebdeb1576a07675a476593ba6dfbd
Yousef eslam <yousefeslam214@gmail.com>
2024-08-14 13:27:22 +0300

    update

--- 0x02-redis_basic/main.py
+++ 0x02-redis_basic/main.py
@@ -1,30 +1,50 @@
 #!/usr/bin/env python3
-"""
-Main file
-"""
-import redis
+""" Main file """
 
 Cache = __import__('exercise').Cache
 
 cache = Cache()
 
-data = b"hello"
-key = cache.store(data)
-print(key)
+cache.store(b"first")
+print(cache.get(cache.store.__qualname__))
 
-local_redis = redis.Redis()
-print(local_redis.get(key))
-print(cache.get(key))
+cache.store(b"second")
+cache.store(b"third")
+print(cache.get(cache.store.__qualname__))
 
+
+
+
+# #!/usr/bin/env python3
+# """
+# Main file
+# """
+# import redis
+
+# Cache = __import__('exercise').Cache
 
 # cache = Cache()
 
-TEST_CASES = {
-    b"foo": None,
-    123: int,
-    "bar": lambda d: d.decode("utf-8")
-}
+# data = b"hello"
+# key = cache.store(data)
+# print(key)
 
-for value, fn in TEST_CASES.items():
-    key = cache.store(value)
-    assert cache.get(key, fn=fn) == value
+# local_redis = redis.Redis()
+# print(local_redis.get(key))
+# print(cache.get(key))
+
+
+# # cache = Cache()
+
+# TEST_CASES = {
+#     b"foo": None,
+#     123: int,
+#     "bar": lambda d: d.decode("utf-8")
+# }
+
+# for value, fn in TEST_CASES.items():
+#     key = cache.store(value)
+#     assert cache.get(key, fn=fn) == value
+
+
+
